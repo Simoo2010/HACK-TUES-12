59ee3818cbecec0b5f4de10feed017cda3d967db
DeyanGonkov <gonkovdeyan@gmail.com>
2026-03-26 14:38:58 +0200

    Cammera update

--- CameraDataCollection.js
+++ CameraDataCollection.js
@@ -5,6 +5,9 @@ async function startCamera(){
          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         const video = document.getElementById("camera");
         video.srcObject = stream;
+
+        document.querySelector('.camera-placeholder-icon').style.display = 'none';
+
         } catch (err) {
         console.error("Camera access denied:", err);
     }
